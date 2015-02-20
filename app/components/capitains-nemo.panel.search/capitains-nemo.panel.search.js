f35f384069c80d62259ac3bb0c356710916a12e1
Thibault Clérice <leponteineptique@gmail.com>
2015-02-20 11:58:59 +0100

    Promisify Search Lunr

--- app/components/capitains-nemo.panel.search/capitains-nemo.panel.search.js
+++ app/components/capitains-nemo.panel.search/capitains-nemo.panel.search.js
@@ -10,7 +10,8 @@ angular
           texts: '=',
           target: '='
         },
-        controller: function ($scope) {
+        controller: ['$scope', '$q',function ($scope, $q) {
+          console.log($q)
           if(typeof $scope.layout.categories.search === "undefined") {
             $scope.layout.categories.search = false;
           }
@@ -19,19 +20,40 @@ angular
           $scope.lunr = null;
 
           var indexing = function() {
-            $scope.lunr = lunr(function () {
-              this.field('title', {boost: 10})
-              this.field('lang', {boost: 1})
-              this.field('author', {boost: 10})
-              this.ref('urn')
-            })
-            angular.forEach($scope.texts, function(text) {
-              text.title = text.getTitle();
-              $scope.lunr.add(text);
-              $scope.index[text.urn] = text;
-            });
+            var deferred = $q.defer();
+            (function() {
+              $scope.lunr = lunr(function () {
+                this.field('title', {boost: 10})
+                this.field('lang', {boost: 1})
+                this.field('author', {boost: 10})
+                this.ref('urn')
+              })
+              angular.forEach($scope.texts, function(text) {
+                text.title = text.getTitle();
+                $scope.lunr.add(text);
+                $scope.index[text.urn] = text;
+              });
+              deferred.resolve();
+            })();
+            return deferred.promise;
           }
+          /**
+           * Placeholder function ulti
+           * @return {[type]} [description]
+           */
           $scope.search = function() {
+            if($scope.texts.length === 0) {
+              return [];
+            }
+            if($scope.lunr === null) {
+              indexing().then(function() {
+              $scope.search = search;
+              $scope.loading = false;
+              $scope.search();
+            });
+            }
+          }
+          var search = function() {
             var results = [];
             if($scope.texts.length === 0) {
               return [];
@@ -50,7 +72,8 @@ angular
             $scope.target = object;
             $scope.layout.select('passages');
           }
-        },
+          $scope.loading = true;
+        }],
         templateUrl: 'components/capitains-nemo.panel.search/capitains-nemo.panel.search.html'
       };
 });
