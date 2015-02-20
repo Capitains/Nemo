caa6a7f1f61206a5531ff7cbab95cffc80d94d5b
Thibault Clérice <leponteineptique@gmail.com>
2015-02-20 07:12:07 +0100

    Separating Capitains Sparrow Passage and Repo

--- app/components/capitains-sparrow/capitains-sparrow.js
+++ app/components/capitains-sparrow/capitains-sparrow.js
@@ -1,42 +1,42 @@
 angular.module('capitainsSparrow.models', [])
   .factory('Repository', ['$q', function($q) {
     //We create a repo
-    var CTSrepo =  {
-      repository : function(endpoint, version) { 
-        return {
-          load : function() {
-            var deferred = $q.defer();
-            this.Repository.load(
-                function() {
-                  deferred.resolve();
-                },
-                function(error) {
-                  deferred.reject("An error occured while fetching items");
-                }
-            );
-            return deferred.promise;
-          },
-          Repository : new CTS.repository.repository(endpoint, 3)
-        }
-      },
-      passage : function(urn, endpoint, inventory) {
-        return {
-          Passage : new CTS.text.Passage(urn, endpoint, inventory),
-          load : function() {
-            var deferred = $q.defer();
-            this.Passage.retrieve({
-              success : function() {
-                  deferred.resolve();
-                },
-              error : function(error) {
-                  deferred.reject("An error occured while fetching items");
-                }
+    var CTSrepo =  function(endpoint, version) { 
+      return {
+        load : function() {
+          var deferred = $q.defer();
+          this.Repository.load(
+              function() {
+                deferred.resolve();
+              },
+              function(error) {
+                deferred.reject("An error occured while fetching items");
               }
-            );
-            return deferred.promise;
-          }
-        }
+          );
+          return deferred.promise;
+        },
+        Repository : new CTS.repository.repository(endpoint, 3)
       }
     }
     return CTSrepo;
+  }])
+  .factory('Passage', ['$q', function($q) {
+    return function(urn, endpoint, inventory) {
+      return {
+        Passage : new CTS.text.Passage(urn, endpoint, inventory),
+        load : function() {
+          var deferred = $q.defer();
+          this.Passage.retrieve({
+            success : function() {
+                deferred.resolve();
+              },
+            error : function(error) {
+                deferred.reject("An error occured while fetching items");
+              }
+            }
+          );
+          return deferred.promise;
+        }
+      }
+    }
   }]);
