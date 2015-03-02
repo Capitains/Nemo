84853d728395a91f1e4dd8b2bd745d0d76b08d20
Thibault Clérice <leponteineptique@gmail.com>
2015-03-02 17:50:34 +0100

    Added support for LocalStorage

--- app/app.js
+++ app/app.js
@@ -1,21 +1,31 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('capitainsNemo', [
-  'ngRoute',
+angular
+  .module('capitainsNemo', [
+    'ngRoute',
+    'LocalStorageModule', 
 
-  'arethusa.core',
+    'arethusa.core',
 
-  'capitainsSparrow.models',
-  'capitainsNemo.home',
-  'capitainsNemo.utils',
-  'capitainsNemo.panels',
-  'capitainsNemo.square',
-  'capitainsNemo.reading',
-  'arethusa.tei',
-  'sanitizeHTML'
-]).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-    .otherwise({redirectTo: '/'});
-  }]);
+    'capitainsSparrow.models',
+    'capitainsNemo.home',
+    'capitainsNemo.utils',
+    'capitainsNemo.panels',
+    'capitainsNemo.square',
+    'capitainsNemo.reading',
+    'arethusa.tei',
+    'sanitizeHTML'
+  ]).
+  config([
+    '$routeProvider',
+    'localStorageServiceProvider', 
+    function($routeProvider, localStorageServiceProvider) {
+      //Routes
+      $routeProvider
+        .otherwise({redirectTo: '/'});
+
+      //Configurations
+      localStorageServiceProvider.setPrefix('capitainsNemo');
+    }
+  ]);
